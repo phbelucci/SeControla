9bceeacd680d72bbb28297d280e4970b06bdbc2c
Paulo Henrique Belucci <paulo.belucci@ibm.com>
2019-05-01 22:34:51 -0300

    o que esta feito do programa em android studio

---
+++ DESENVOLVIMENTO/SeControla/app/src/main/java/com/example/primeiroprojeto/defineCategoria.java
@@ -0,0 +1,13 @@
+package com.example.primeiroprojeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class defineCategoria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_define_categoria);
+    }
+}
---
+++ DESENVOLVIMENTO/SeControla/app/src/main/java/com/example/primeiroprojeto/inserirValores.java
@@ -0,0 +1,19 @@
+package com.example.primeiroprojeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class inserirValores extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inserir_valores);
+
+
+    }
+}
