18e7ce3eb7c3fa6391d42f9b563f6b5df2edea14
adila.fattobene@ifood.com.br <adila.fattobene@ifood.com.br>
2019-05-27 14:01:56 -0300

    adicionando entidades

---
+++ DESENVOLVIMENTO/SeControla/app/src/main/java/com/example/primeiroprojeto/model/model/SubCategoria.java
@@ -0,0 +1,27 @@
+package com.example.primeiroprojeto.model;
+
+
+public final class SubCategoria{
+    private Integer codSubCat;
+    private Integer codCat;
+    //private com.example.primeiroprojeto.model.Categoria tipoLancamento;
+    private String nome;
+
+    public SubCategoria(Integer codSubCat, Integer codCat, String nome) {
+        this.codSubCat = codSubCat;
+        this.codCat = codCat;
+        this.nome = nome;
+    }
+
+    public Integer getCodSubCat() {
+        return codSubCat;
+    }
+
+    public Integer getCodCat() {
+        return codCat;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
