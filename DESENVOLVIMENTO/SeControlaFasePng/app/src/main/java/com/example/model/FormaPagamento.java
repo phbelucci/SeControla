ca8dd30f716a4aa3d6db9c0008c6145f8a1ce034
SilvioDaCol <silviocbrito@hotmail.com>
2019-05-28 01:05:09 -0300

    Organizacao das pastas do projeto

---
+++ DESENVOLVIMENTO/SeControlaFasePng/app/src/main/java/com/example/model/FormaPagamento.java
@@ -0,0 +1,15 @@
+package com.example.primeiroprojeto.model;
+
+public enum FormaPagamento{
+    DINHEIRO(1),
+    DEBITO (2),
+    CREDITO(3),
+    TRANSFERENCIA(4),
+    CHEQUE(5);
+
+    public int opcao;
+
+    FormaPagamento(int opcao) {
+        this.opcao = opcao;
+    }
+}
