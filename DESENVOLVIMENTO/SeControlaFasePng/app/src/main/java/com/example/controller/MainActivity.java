1edf720558014597037703a94e43c204ec6d1b7e
Paulo Henrique Belucci <paulo.belucci@ibm.com>
2019-05-30 04:39:08 -0300

    tela de manter lançamentos

--- DESENVOLVIMENTO/SeControlaFasePng/app/src/main/java/com/example/controller/MainActivity.java
+++ DESENVOLVIMENTO/SeControlaFasePng/app/src/main/java/com/example/controller/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_novo_lancamento_vw);
+        setContentView(R.layout.activity_manter_lancamentos_vw);
 
 
 
---
+++ DESENVOLVIMENTO/SeControlaFasePng/app/src/main/java/com/example/controller/manter_lancamentos_vw.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class manter_lancamentos_vw extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manter_lancamentos_vw);
+    }
+}
