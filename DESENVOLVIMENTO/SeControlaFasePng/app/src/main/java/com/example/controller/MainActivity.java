c1bc1a6d1c77067b3129c94274663c64f6994274
Paulo Henrique Belucci <paulo.belucci@ibm.com>
2019-05-31 13:48:44 -0300

    botões chamando telas - parte 1

--- DESENVOLVIMENTO/SeControlaFasePng/app/src/main/java/com/example/controller/MainActivity.java
+++ DESENVOLVIMENTO/SeControlaFasePng/app/src/main/java/com/example/controller/MainActivity.java
@@ -13,20 +13,49 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_resumo_lancamento_vw);
+        setContentView(R.layout.activity_login_vw);
 
 
 
     }
-    public void incluirNovoGasto(View view){
+    public void chamarTelaCategoriaGastos(View view){
         Intent intent = new Intent(this, escolherCatGastos_vw.class);
         startActivity(intent);
     }
 
-    public void incluirNovaReceita(View view){
+    public void chamarTelaCategoriaReceitas(View view){
 
         Intent intent = new Intent(this, escolherCatReceitas_vw.class);
         startActivity(intent);
     }
+
+    public void chamarTelaInicio(View view){
+
+        Intent intent = new Intent(this, inicio_vw.class);
+        startActivity(intent);
+    }
+
+    public void chamarTelaCadastro(View view){
+
+        Intent intent = new Intent(this, cadastro_vw.class);
+        startActivity(intent);
+    }
+
+    public void chamarTelaManterDependente(View view){
+
+        Intent intent = new Intent(this, manterDependente_vw.class);
+        startActivity(intent);
+    }
+
+    public void chamarTelaManterPerfil(View view){
+
+        Intent intent = new Intent(this, manterPerfil_vw.class);
+        startActivity(intent);
+    }
+
+
+
+
+
 }
 
