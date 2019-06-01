89792f146fecefbc4d2a4b692548943c912e1674
SilvioDaCol <silviocbrito@hotmail.com>
2019-06-01 19:21:32 -0300

    Modificacoes banco de dados, novo DER e novo diagrama MVC

--- DATABASE/db_secontrola_create.sql
+++ DATABASE/db_secontrola_create.sql
@@ -2,42 +2,43 @@ drop database if exists db_secontrola;
 create database db_secontrola;
 use db_secontrola;
 
-create table COTACAO
+create table MOEDA
 (
 	COD_MOEDA int auto_increment primary key,
-	DESCR_MOEDA varchar(10) not null,
-	VALOR_EM_REAIS real not null
+	DESCR_MOEDA varchar(30) not null,
+	COTACAO_REAIS real not null
 
 );
 
-create table NIVEL_ACESSO
+create table LOGIN
 (
-	COD_NIVEL_AC int auto_increment primary key,
-	DESCR_NIVEL_AC varchar(20) not null
+	COD_US int auto_increment primary key,
+	NOME_US varchar(200) not null,
+	SENHA_US varchar(20) not null
+);
 
+create table GRUPO_FAMILIAR(
+	COD_GRUPO int auto_increment primary key,
+	COD_ADM_GRUPO int,
+	COD_MOEDA_PADRAO int default 1
 );
 
 create table USUARIO
 (
-	COD_US int auto_increment primary key,
-	NOME_US varchar(200) not null,
-	SENHA_US varchar(20) not null,
+	COD_US int primary key,
 	AVATAR_US longblob,
-	COD_MOEDA int default 1 not null,
 	COD_NIVEL_ACESSO int not null, 
-	COD_US_GRUPO int default null,
+	COD_GRUPO int default null,
 
-	constraint foreign key FK_MOEDA(COD_MOEDA) references COTACAO(COD_MOEDA) on delete cascade,
-    constraint foreign key FK_NIVEL_AC(COD_NIVEL_ACESSO) references NIVEL_ACESSO(COD_NIVEL_AC) on delete cascade,
-	constraint foreign key FK_US_GRUPO(COD_US_GRUPO) references USUARIO(COD_US) on delete cascade
+	constraint foreign key FK_US_LOGIN(COD_US) references LOGIN(COD_US) on delete cascade,
+	constraint foreign key FK_GRUPO(COD_GRUPO) references GRUPO_FAMILIAR(COD_GRUPO) on delete cascade
 );
 
 create table CATEGORIA
 (
 	COD_CAT int auto_increment primary key,
-	TIPO_CAT varchar(20) not null,
+	TIPO_LANC_ENUM varchar(20) not null,
 	DESCR_CAT varchar(20) not null
-
 );
 
 
@@ -53,59 +54,36 @@ create table SUBCATEGORIAS
 create table CONTA_BANCO
 (
 	COD_CONTA int auto_increment primary key,
-	COD_US int not null,
+	COD_GRUPO int not null,
 	NUM_CONTA int not null,
 	NUM_AGENCIA int not null,
+	COD_MOEDA int default 1,
 	SALDO real default 0.00,
     
-    constraint foreign key FK_BANC_US(COD_US) references USUARIO(COD_US) on delete cascade
+    constraint foreign key FK_BANC_GRUPO(COD_GRUPO) references GRUPO_FAMILIAR(COD_GRUPO) on delete cascade
 );
 
-create table FORMA_PGTO
+create table LANCAMENTO
 (
-	COD_FORMA_PGTO int auto_increment primary key,
-	DESCR_FORMA_PGTO varchar(20) not null
-
-);
-
-create table RECEITA
-(
-	COD_REC int auto_increment primary key,
+	COD_LANC int auto_increment primary key,
 	COD_US int not null,
-	DATA_REC datetime not null,
+	COD_GRUPO int not null,
+	TIPO_LANC_ENUM varchar(20) not null,
+	DATA_LANC datetime not null,
 	COD_CAT int not null,
 	COD_SUBCAT int default null,
 	COMPROVANTE mediumblob , 
 	VALOR real not null,
  	COD_CONTA int default null,
 	COD_FORMA_PGTO int default null,
+	COD_MOEDA int default 1,
 
-	constraint foreign key FK_REC_US(COD_US) references USUARIO(COD_US) on delete cascade,
-    constraint foreign key FK_REC_CAT(COD_CAT) references CATEGORIA(COD_CAT) on delete cascade,
-    constraint foreign key FK_REC_SUBCAT(COD_SUBCAT) references SUBCATEGORIAS(COD_SUBCAT) on delete cascade,
-    constraint foreign key FK_REC_CONTA(COD_CONTA) references CONTA_BANCO(COD_CONTA) on delete cascade,
-    constraint foreign key FK_REC_FORMA_PGTO(COD_FORMA_PGTO) references FORMA_PGTO(COD_FORMA_PGTO) on delete cascade
+	constraint foreign key FK_LANC_US(COD_US) references USUARIO(COD_US) on delete cascade,
+	constraint foreign key FK_LANC_GRUPO(COD_GRUPO) references GRUPO_FAMILIAR(COD_GRUPO) on delete cascade,
+    constraint foreign key FK_LANC_CAT(COD_CAT) references CATEGORIA(COD_CAT) on delete cascade,
+    constraint foreign key FK_LANC_SUBCAT(COD_SUBCAT) references SUBCATEGORIAS(COD_SUBCAT) on delete cascade,
+    constraint foreign key FK_LANC_CONTA(COD_CONTA) references CONTA_BANCO(COD_CONTA) on delete cascade,
+	constraint foreign key FK_LANC_MOEDA(COD_MOEDA) references MOEDA(COD_MOEDA) on delete cascade
 );
-
-create table DESPESA
-(
-	COD_DESP int auto_increment primary key,
-	COD_US int not null,
-	DATA_DESP datetime not null,
-	COD_CAT int not null,
-	COD_SUBCAT int default null,
-	COMPROVANTE mediumblob , 
-	VALOR real not null,
- 	COD_CONTA int default null,
-	COD_FORMA_PGTO int default null,
-
-	constraint foreign key FK_DESP_USUARIO(COD_US) references USUARIO(COD_US) on delete cascade,
-    constraint foreign key FK_DESP_CAT(COD_CAT) references CATEGORIA(COD_CAT) on delete cascade,
-    constraint foreign key FK_DESP_SUBCAT(COD_SUBCAT) references SUBCATEGORIAS(COD_SUBCAT) on delete cascade,
-    constraint foreign key FK_DESP_CONTA(COD_CONTA) references CONTA_BANCO(COD_CONTA) on delete cascade,
-    constraint foreign key FK_DESP_FORMA_PGTO(COD_FORMA_PGTO) references FORMA_PGTO(COD_FORMA_PGTO) on delete cascade
-);
-
-
 
 
