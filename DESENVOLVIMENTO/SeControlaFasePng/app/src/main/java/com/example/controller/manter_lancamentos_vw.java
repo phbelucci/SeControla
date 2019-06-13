c4ddb4a5adfde8164ae4a001895d37f090fe56af
Silvio da Col <40775733+SilvioDaCol@users.noreply.github.com>
2019-06-13 12:43:54 -0300

    conflito tela manter lancamentos

--- DESENVOLVIMENTO/SeControlaFasePng/app/src/main/java/com/example/controller/manter_lancamentos_vw.java
+++ DESENVOLVIMENTO/SeControlaFasePng/app/src/main/java/com/example/controller/manter_lancamentos_vw.java
@@ -1,32 +1,27 @@
 package com.example.controller;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.example.entity.GrupoFamiliar;
+import com.example.entity.Lancamento;
 import com.example.entity.Usuario;
 import com.example.model.LancamentoModel;
-import com.example.model.ListaAdapterLancamento;
+//import com.example.model.ListaAdapterLancamento;
 import java.util.ArrayList;
 
 
 public class manter_lancamentos_vw extends AppCompatActivity {
 
     ListView lista;
-    ArrayList<LancamentoModel> a = new ArrayList<LancamentoModel>();
     private int tipo;
     private Usuario u;
     private GrupoFamiliar g;
-    private AlertDialog alerta;
 
 
     @Override
@@ -43,43 +38,18 @@ public class manter_lancamentos_vw extends AppCompatActivity {
         g = (GrupoFamiliar) pegaDados.getSerializable("Grupo");
         System.out.println("TESTE: " + u.getNomeUs());
 
-
-
-
-
-
-        /********************
-         *
-         *
-         *       Função que retorna Lista de lançamentos do grupo
-         *       g.getLancamentosGrupo();
-         *
-         *       Função para adicionar lancamento à lista de lacamentos
-         *       g.addLancamentosGrupo(lancamento);
-         *
-         *
-         * *******************/
-
-
-
-
-        //getSerializable pega o valor do parametro, atraves da chave informada na outra tela
-        //neste caso getSerializable, mas poderia ser getInt, getString, etc.
-        //LancamentoModel novo = (LancamentoModel) pegaLancamento.getSerializable("novoGasto");
-
-        ArrayList<LancamentoModel> novoArrayG = (ArrayList<LancamentoModel>) pegaDados.getSerializable("ArrayG");
-        ArrayList<LancamentoModel> novoArrayR = (ArrayList<LancamentoModel>) pegaDados.getSerializable("ArrayR");
-
+        /*
         ListaAdapterLancamento adaptador;
         ListView listView = (ListView) findViewById(R.id.listaLancamentos);
+        /*
         if(tipo == 1){
-            adaptador = new ListaAdapterLancamento(this,novoArrayG);
+            adaptador = new ListaAdapterLancamento(this, g.getLancamentosGrupo());
             listView.setAdapter(adaptador);
         } if (tipo == 2){
             adaptador = new ListaAdapterLancamento(this,novoArrayR);
             listView.setAdapter(adaptador);
         }
-
+        */
         if(tipo ==1){
             Button Gasto = findViewById(R.id.btnListarDespesas);
             Gasto.setBackgroundColor(Color.parseColor("#FA001A"));
@@ -136,33 +106,5 @@ public class manter_lancamentos_vw extends AppCompatActivity {
         startActivity(intent);
 
     }
-    public void chamaAlerta(View view){
-
-
-        LayoutInflater li = getLayoutInflater();
-        View v = li.inflate(R.layout.detalharlancamento, null);
-        v.findViewById(R.id.btnSairDetalhes).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
-                //exibe um Toast informativo.
-                Toast.makeText(manter_lancamentos_vw.this, "alerta.dismiss()", Toast.LENGTH_SHORT).show();
-                //desfaz o alerta.
-                alerta.dismiss();
-            }
-        });
-        Lancamento l = new Lancamento();
-        //Cria o gerador do AlertDialog
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        //define o titulo
-        builder.setView(v);
-        //define a mensagem
-
-
-        //cria o AlertDialog
-        alerta = builder.create();
-        //Exibe
-        alerta.show();
-
-    }
-
 
 }
